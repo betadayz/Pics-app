e34eba5cef0959221de221150ad1aa51d354745b
Beta Dayz <betadayz888@gmail.com>
2020-02-03 17:59:52 +0100

    Data fetching manipulation

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,24 @@ import SearchBar from './SearchBar';
 
 
 export default class App extends Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    state = { images: [] };
+    
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 178dbb60966f5323fe01df7d3de7ddc2f6dabf820f7a2bde8c381a360c8d7a68'
             }
-        })
+        });
+        console.log(this)
+        this.setState({ images: response.data.results });
     }
+    
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
             <SearchBar onSubmit={this.onSearchSubmit} />
+            Found: {this.state.images.length} images
             </div>
         )
     }
