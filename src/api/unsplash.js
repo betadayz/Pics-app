9f4cc660483659ed3b41a6d2e802fb062b9b67a0
Beta Dayz <betadayz888@gmail.com>
2020-02-03 18:21:18 +0100

    Data fetching manipulation

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID 178dbb60966f5323fe01df7d3de7ddc2f6dabf820f7a2bde8c381a360c8d7a68'
+    }
+})
--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 
 export default class App extends Component {
     state = { images: [] };
     
-    async onSearchSubmit(term) {
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
+   onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
             params: { query: term },
-            headers: {
-                Authorization: 'Client-ID 178dbb60966f5323fe01df7d3de7ddc2f6dabf820f7a2bde8c381a360c8d7a68'
-            }
         });
-        console.log(this)
+
         this.setState({ images: response.data.results });
     }
-    
+
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
